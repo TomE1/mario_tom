7f4d2088f3dc5699f84950c9daf61470af8a6270
TomE <tommevans210@gmail.com>
2023-08-05 08:54:40 +0700

    gravity is done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+const gravity = 0.5;
 
 class Player {
   constructor() {
@@ -12,7 +12,7 @@ class Player {
     };
     this.velocity = {
       x: 0,
-      y: 1,
+      y: 0,
     };
     this.width = 30;
     this.height = 30;
@@ -22,16 +22,20 @@ class Player {
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
-    this.position.y += this.velocity.y;
     this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
   }
 }
 
 const player = new Player();
-player.update();
 
 function animate() {
   requestAnimationFrame(animate);
-  console.log('go');
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
 }
 animate();

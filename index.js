717c04bdbf3eb84e20c0e3440761cc1a672e7119
TomE <tommevans210@gmail.com>
2023-08-05 08:36:49 +0700

    first one

---
+++ index.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.position.y += this.velocity.y;
+    this.draw();
+  }
+}
+
+const player = new Player();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  console.log('go');
+}
+animate();
